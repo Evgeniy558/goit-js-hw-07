e83ca65f7f3fd77b2514b2ef1c813dbedf669908
Evgenij Prishchepov <prishchepovee@gmail.com>
2023-04-01 16:51:47 +0300

    hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+
+function creatGallery(array) {
+  const arrayImg = [];
+  array.forEach((obj) => {
+    const liEl = document.createElement("li");
+    liEl.classList.add("gallery__item");
+
+    const anchorEl = document.createElement("a");
+    anchorEl.classList.add("gallery__link");
+    anchorEl.href = obj.original;
+
+    const imgEl = document.createElement("img");
+    imgEl.classList.add("gallery__image");
+    imgEl.src = obj.preview;
+    imgEl.dataset.source = obj.original;
+    imgEl.alt = obj.description;
+
+    liEl.append(anchorEl);
+    anchorEl.append(imgEl);
+    arrayImg.push(liEl);
+  });
+  return arrayImg;
+}
+
+ulEl.prepend(...creatGallery(galleryItems));
+
+ulEl.onclick = (ev) => {
+  if (ev.target.nodeName !== "IMG") {
+    return;
+  }
+  ev.preventDefault();
+  const modal = basicLightbox.create(
+    `<img width="1400" height="900" src=${ev.target.dataset.source}>`
+  );
+  modal.show();
+
+  document.addEventListener("keydown", (ev) => {
+    console.log(ev.code);
+    if (ev.code === "Escape") {
+      modal.close();
+    }
+  });
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+
+function creatGallery(array) {
+  const arrayImg = [];
+  array.forEach((obj) => {
+    const liEl = document.createElement("li");
+    liEl.classList.add("gallery__item");
+
+    const anchorEl = document.createElement("a");
+    anchorEl.classList.add("gallery__link");
+    anchorEl.href = obj.original;
+
+    const imgEl = document.createElement("img");
+    imgEl.classList.add("gallery__image");
+    imgEl.src = obj.preview;
+    imgEl.alt = obj.description;
+    imgEl.title = obj.description;
+
+    liEl.append(anchorEl);
+    anchorEl.append(imgEl);
+    arrayImg.push(liEl);
+  });
+  return arrayImg;
+}
+ulEl.prepend(...creatGallery(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
+
+ulEl.addEventListener("click", (ev) => {
+  if (ev.target.nodeName !== "IMG") {
+    return;
+  }
+  ev.preventDefault();
+});
